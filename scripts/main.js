428f7a6785ea788737de2963559abdf74078f833
aravk <aravkasi@gmail.com>
2022-08-27 09:10:09 -0500

    Remove floating navbar

--- scripts/main.js
+++
@@ -1,7 +0,0 @@
-function showMenu() {
-  document.querySelector('#menu').className = document.documentElement.scrollTop >= document.querySelector('#about').offsetTop ? '' : 'hide';
-}
-
-showMenu();
-
-window.onscroll = showMenu;
